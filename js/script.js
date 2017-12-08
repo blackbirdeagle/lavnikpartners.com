2efb2c8b0f7562dd330858c028dbaeac820d7806
Alexander <Alexander>
2017-12-08 14:06:58 +0300

    Еще 4-е страницы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
+function isEmailValid(emailAdress) {
+	var EMAIL_REGEXP = new RegExp('^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$', 'i');
+		return EMAIL_REGEXP.test(emailAdress)
+}
+
 $(".no__car__list li a").click(function(){
 	$(".no__car__list li p").hide();
 	$(this).siblings('p').fadeIn(200);
+});
+
+$('.vac a').click(function(){
+	var vac = $(this).attr('vac');
+
+	$('.vac div').hide();
+	
+	$('#'+vac).fadeIn(200);
 });
 
 $('.politik').click(function(){
@@ -62,6 +75,62 @@ $('.hide__menu').click(function(){
 
 jQuery(function(){
 	jQuery(".phone").mask("+7(999) 999-9999");
+});
+
+$('.send_quest').click(function(){
+	var quest = $('.quest__form input[name = "quest"]').val();
+	var email = $('.quest__form input[name = "email"]').val();
+	var check = $('.politik').attr('check');
+	var flag = 0;
+	
+	$('.quest__form input').css({"border":"1px solid #a18c6f"});
+	$('.politik').css({"border":"none"});
+	
+	if(quest == ""){
+		$('.quest__form input[name = "quest"]').css({"border":"1px solid red"});
+		flag = 1;
+	}
+	
+	if(email == ""){
+		$('.quest__form input[name = "email"]').css({"border":"1px solid red"});
+		flag = 1;
+	}
+	
+	if(email != "" && !isEmailValid(email)){
+		$('.quest__form input[name = "mail"]').css({"border":"1px solid red"});
+		flag = 1;
+	}
+	
+	if(check == "no"){
+		$('.politik').css({"border":"1px solid red"});
+		flag = 1;
+	}
+	
+	if(flag == 0){
+		
+	}
+});
+
+$('.send_mess').click(function(){
+	var name = $('.popup input[name = "name"]').val();
+	var phone = $('.popup input[name = "phone"]').val();
+	var flag = 0;
+	
+	$('.popup input').css({"border":"none"});
+	
+	if(name == ""){
+		$('.popup input[name = "name"]').css({"border":"1px solid red"});
+		flag = 1;
+	}
+	
+	if(phone == ""){
+		$('.popup input[name = "phone"]').css({"border":"1px solid red"});
+		flag = 1;
+	}
+	
+	if(flag == 0){
+		
+	}
 });
 /*
 var myMap;
